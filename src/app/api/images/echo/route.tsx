1dd9239deb6b4abc1a61e0963a2c73e9867d3f34
earth2travis <travis.mccutcheon@gmail.com>
2024-02-03 20:37:17 -0600

    Remove image

--- src/app/api/images/echo/route.tsx
+++ src/app/api/images/echo/route.tsx
@@ -1,68 +1,60 @@
-import { NextRequest } from "next/server";
-import { ImageResponse } from "next/og";
-import { join } from "path";
-import * as fs from "fs";
+import { NextRequest } from 'next/server';
+import { ImageResponse } from 'next/og';
+import { join } from 'path';
+import * as fs from 'fs';
 
-export const dynamic = "force-dynamic";
+export const dynamic = 'force-dynamic';
 
-const interRegPath = join(process.cwd(), "public/Inter-Regular.ttf");
+const interRegPath = join(process.cwd(), 'public/Inter-Regular.ttf');
 let interReg = fs.readFileSync(interRegPath);
 
-const interBoldPath = join(process.cwd(), "public/Inter-Bold.ttf");
+const interBoldPath = join(process.cwd(), 'public/Inter-Bold.ttf');
 let interBold = fs.readFileSync(interBoldPath);
 
 export async function GET(req: NextRequest) {
   const searchParams = req.nextUrl.searchParams;
-  const message = searchParams.get("message") ?? "";
+  const message = searchParams.get('message') ?? '';
   return new ImageResponse(
     (
       <div
         style={{
-          display: "flex", // Use flex layout
-          flexDirection: "row", // Align items horizontally
-          alignItems: "stretch", // Stretch items to fill the container height
-          width: "100%",
-          height: "100vh", // Full viewport height
-          backgroundColor: "white",
+          display: 'flex', // Use flex layout
+          flexDirection: 'row', // Align items horizontally
+          alignItems: 'stretch', // Stretch items to fill the container height
+          width: '100%',
+          height: '100vh', // Full viewport height
+          backgroundColor: 'white',
         }}
       >
-        <img
-          style={{
-            height: "100%", // Make image full height
-            objectFit: "cover", // Cover the area without losing aspect ratio
-            width: "35%", // Image takes up 40% of the container's width
-          }}
-          src="https://imagedelivery.net/BXluQx4ige9GuW0Ia56BHw/2639523a-690b-47af-16ab-ca07697fd000/original"
-        />
         <div
           style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "flex-start",
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'flex-start',
             paddingLeft: 24,
             paddingRight: 24,
             lineHeight: 1.2,
             fontSize: 36,
-            color: "black",
+            color: 'black',
             flex: 1,
-            overflow: "hidden",
+            overflow: 'hidden',
             marginTop: 24,
           }}
         >
           <div
             style={{
-              color: "#0a588c",
+              color: '#0a588c',
               fontSize: 72,
               marginBottom: 12,
-              display: "flex"
+              display: 'flex',
             }}
           >
             <strong>Echo Says...</strong>
           </div>
           <div
             style={{
-              display: "flex",
-              overflow: "hidden",
+              display: 'flex',
+              overflow: 'hidden',
             }}
           >
             &quot;{message}&quot;
@@ -75,16 +67,16 @@ export async function GET(req: NextRequest) {
       height: 800,
       fonts: [
         {
-          name: "Inter",
+          name: 'Inter',
           data: interReg,
           weight: 400,
-          style: "normal",
+          style: 'normal',
         },
         {
-          name: "Inter",
+          name: 'Inter',
           data: interBold,
           weight: 800,
-          style: "normal",
+          style: 'normal',
         },
       ],
     }
