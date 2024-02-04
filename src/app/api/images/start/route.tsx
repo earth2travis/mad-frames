98387398be24b8d0b130966783f40545cc7ffee1
earth2travis <travis.mccutcheon@gmail.com>
2024-02-03 19:41:02 -0600

    Restore original image

--- src/app/api/images/start/route.tsx
+++ src/app/api/images/start/route.tsx
@@ -26,9 +26,9 @@ export async function GET() {
           style={{
             height: '100%', // Make image full height
             objectFit: 'cover', // Cover the area without losing aspect ratio
-            width: '40%', // Image takes up 40% of the container's width
+            width: '35%', // Image takes up 40% of the container's width
           }}
-          src="https://mad-frames.vercel.app/banality.png"
+          src="https://imagedelivery.net/BXluQx4ige9GuW0Ia56BHw/2639523a-690b-47af-16ab-ca07697fd000/original"
         />
         <div
           style={{
@@ -53,7 +53,7 @@ export async function GET() {
               display: 'flex',
             }}
           >
-            <strong>Name of Person</strong>
+            <strong>Echo The Dolphin</strong>
           </div>
           <div
             style={{
@@ -61,7 +61,7 @@ export async function GET() {
               overflow: 'hidden',
             }}
           >
-            Type a name in the text input below.
+            Type something in the text input below and Echo will say it back.
           </div>
         </div>
       </div>
